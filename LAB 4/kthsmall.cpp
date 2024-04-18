31bdc3854019139cbf5c7e1f0077954c47d39d11
michuishaan07 <158288486+michuishaan07@users.noreply.github.com>
2024-04-18 14:52:03 +0530

    Update kthsmall.cpp

--- LAB 4/kthsmall.cpp
+++ LAB 4/kthsmall.cpp
@@ -1,1 +1,54 @@
+#include<iostream>
+#include<chrono>
+#include<vector>
+#include<cstdlib>
 
+
+using namespace std:: chrono;
+
+using namespace std;
+
+
+int partition(int arr[],int low ,int high)
+{
+    
+int i=0;
+int j=high-1;
+int pivot=arr[low];
+
+do{
+
+    do{
+        i=i++;
+    }
+    while(arr[i]<pivot);
+
+
+    do{
+        j=j--;
+    }while(arr[j]>pivot);
+
+
+if(i<j)
+{
+   swap(arr[i],arr[j]);
+}
+}
+while(i<j);
+
+arr[low]=arr[j];
+
+arr[j]=pivot;
+
+return j;
+
+}
+
+int main()
+{
+
+int arr[5]={13,6,7,8,15};
+int k=partition(arr,0,6);
+cout<<k;
+
+}
